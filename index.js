2c72a77d1732a67506e0d32507ae9a8c9542cdc4
Katia <katia.punter@storyshareplatform.com>
2021-04-28 08:37:23 +0100

    Adds tests and things

---
+++ index.js
@@ -0,0 +1,51 @@
+// Test function minVal with one describe block and several tests and assertions
+// Suggestion: you may want to check what data type is returned
+// Refactor the function so that it does not allow to enter an empty array (throw)
+// Test the refactored function
+
+const minVal = arrVal => Math.min(...arrVal);
+
+// Test function fizzBuzz thoroughly
+// Does it have a logic error?
+// Refactor and test some more
+
+const fizzBuzz = n => {
+    let result = [];
+    for(let i=0;i<n;i++){
+        if(i%3===0){
+            result.push('fizz');
+        } else if(i%5===0){
+            result.push('buzz');
+        } else if(i%5===0 && i%3===0){
+            result.push('fizzbuzz');
+        } else{
+            result.push(i);
+        }
+    }
+  return result;
+}
+
+//Test function timesTwo with different tests and a single describe block
+// You can use isInstanceOf to test if it returns an array
+//Remember that toBe can only be used here on individual elements
+
+const timesTwo = numbersToIncrease => numbersToIncrease.map(value => value*2);
+
+//Test function returnObjects  with different tests and a single describe block
+// You can use isInstanceOf to test if it returns an array
+// You can use isInstanceOf to test each element in that array
+// You can use toHaveProperty() to check each element of the returned array
+
+const returnObjects = arrNames => arrNames.map((name, index) => ({name, id: index+1}));
+
+// Test function removeBMW()
+// Make sure you throw something
+
+function removeBMW(str){
+    if (typeof str !== 'string') {
+        throw new Error("This program only works for text.");
+    }
+    return str.replace(/[bmw]/gi,'');
+}
+
+module.exports = { minVal, fizzBuzz, timesTwo };
---
+++ index.spec.js
@@ -0,0 +1,34 @@
+const { minVal, fizzBuzz, timesTwo } = require('./index');
+test('passes when value is NaN', () => {
+    expect(NaN).toBeNaN();
+    expect(1).not.toBeNaN();
+    expect(typeof NaN).toBe('number');
+});
+
+describe('Testing function minVal()', () => {
+    it('Shoul return a value of data type number', () => {
+        expect(typeof minVal([1,2,34,0,-12])).toBe('number');
+        expect(minVal([1,2,34,0,-12])).not.toBeInstanceOf(Object);
+    });
+    it('Shoul get a minimal value from an array', () => {
+        expect(minVal([])).toBe(-12);
+    });
+});
+
+describe('Testing function timesTwo()', () => {
+    it('returns an array where numbers are doubled', () => {
+        expect(timesTwo([1,2])).toBe([5,4]);
+    });
+});
+
+describe('Testing function fizzBuzz()', () => {
+    it('returns an array of the same length as a parameter', () => {
+        expect(fizzBuzz(10)).toHaveLength(7);
+    });
+    it('returns an array with some fizz, buzz and fizzbuzz', () => {
+        expect(fizzBuzz(3)).toContain('fizz');
+        expect(fizzBuzz(3)).toContain('fizzbuzz');
+    })
+});
+
+
