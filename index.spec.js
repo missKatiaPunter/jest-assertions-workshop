da80412616d51d199fbe2eae3e3f0d45a875082f
Keseana Robinson <64967367+kesh4207@users.noreply.github.com>
2021-04-28 14:59:29 +0100

    testing

--- index.spec.js
+++ index.spec.js
@@ -1,61 +1,65 @@
-const { minVal, fizzBuzz, timesTwo, returnObjects, removeBMW } = require('./index');
+const {
+	minVal,
+	fizzBuzz,
+	timesTwo,
+	returnObjects,
+	removeBMW,
+} = require('./index');
 
 test('passes when value is NaN', () => {
-    expect(NaN).toBeNaN();
-    expect(1).not.toBeNaN();
-    expect(typeof NaN).toBe('number');
+	expect(NaN).toBeNaN();
+	expect(1).not.toBeNaN();
+	expect(typeof NaN).toBe('number');
 });
 
 describe('Testing function minVal()', () => {
-  
-    it('Should throw an error if a parameter is not valid', () => {
-     expect(()=> {
-         minVal([])
-     }).toThrow("enter a valid value"); 
-    });
-    it('Shoul return a value of data type number', () => {
-        expect(typeof minVal([1,2,34,0,-12])).toBe('number');
-        expect(minVal([1,2,34,0,-12])).not.toBeInstanceOf(Object);
-    });
-  
-    it('Should get a minimal value from an array', () => {
-        expect(minVal([1,2,34,0,-12])).toBe(-12);
-    });
-  
+	it('Should throw an error if a parameter is not valid', () => {
+		expect(() => {
+			minVal([]);
+		}).toThrow('enter a valid value');
+	});
+	it('Shoul return a value of data type number', () => {
+		expect(typeof minVal([1, 2, 34, 0, -12])).toBe('number');
+		expect(minVal([1, 2, 34, 0, -12])).not.toBeInstanceOf(Object);
+	});
+
+	it('Should get a minimal value from an array', () => {
+		expect(minVal([1, 2, 34, 0, -12])).toBe(-12);
+	});
 });
 
 describe('Testing function timesTwo()', () => {
-    it('returns an array where numbers are doubled', () => {
-        expect(timesTwo([1,2])).toEqual([2,4]);
-    });
+	it('returns an array where numbers are doubled', () => {
+		expect(timesTwo([1, 2])).toEqual([2, 4]);
+		expect(timesTwo([-1, 2])).toEqual([-2, 4]);
+	});
 });
 
 describe('Testing function fizzBuzz()', () => {
-    it('returns an array of the same length as a parameter', () => {
-        expect(fizzBuzz(10)).toHaveLength(10);
-    });
-    it('returns an array with some fizz, buzz and fizzbuzz', () => {
-        expect(fizzBuzz(3)).toContain('fizz');
-        expect(fizzBuzz(5)).toContain('buzz');
-        expect(fizzBuzz(15)).toContain('fizzbuzz');
-    })
+	it('returns an array of the same length as a parameter', () => {
+		expect(fizzBuzz(10)).toHaveLength(10);
+	});
+	it('returns an array with some fizz, buzz and fizzbuzz', () => {
+		expect(fizzBuzz(3)).toContain('fizz');
+		expect(fizzBuzz(5)).toContain('buzz');
+		expect(fizzBuzz(15)).toContain('fizzbuzz');
+	});
 });
 
 describe('Testing returnobjects()', () => {
-    it('should return an array', () => {
-        expect(returnObjects(['a','b'])).toBeInstanceOf(Object);
-        expect(returnObjects(['a','b'])[0]).toHaveProperty('name');        
-    });
-    
+	it('should return an array', () => {
+		expect(returnObjects(['a', 'b'])).toBeInstanceOf(Object);
+		expect(returnObjects(['a', 'b'])[0]).toHaveProperty('name');
+	});
 });
 
 describe('testing removeBMW()', () => {
-    it('should throw an error if a parameter is invalid ', () => {
-        expect(()=>{
-            removeBMW(100)
-        }).toThrow("This program only works for text.");
-    });
-    it('should remove bmw', () => {
-        expect(removeBMW('Nobmw')).toBe('No');
-    });
+	it('should throw an error if a parameter is invalid ', () => {
+		expect(() => {
+			removeBMW(100);
+		}).toThrow('This program only works for text.');
+	});
+	it('should remove bmw', () => {
+		expect(removeBMW('Nobmw')).toBe('No');
+	});
 });
