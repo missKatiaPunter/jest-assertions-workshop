3d3acaeba36c8f2558f6b7c121e29b13bddbe8e9
Katia <katia.punter@storyshareplatform.com>
2021-12-14 08:32:08 +0000

    Removes and adds

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 // Test the refactored function
 
 const minVal = arrVal => {
-    if(!arrVal || arrVal.length === 0) {
-        throw new Error("Please enter a parameter");
-    }
   return Math.min(...arrVal);
 }
 
@@ -16,13 +13,13 @@ const minVal = arrVal => {
 // Refactor and test some more
 
 const fizzBuzz = n => {
-    let result = [];
+
     for(let i=1; i<=n; i++){
         if(i%3===0 && i%5===0){
             result.push('fizzbuzz');
         } else if(i%3===0){
             result.push('fizz');
-        } else if(i%5===0 && i%3!==0){
+        } else if(i%3===0){
             result.push('buzz');
         } else{
             result.push(i);
@@ -43,16 +40,7 @@ const timesTwo = numbersToIncrease => numbersToIncrease.map(value => value*2);
 // You can use toHaveProperty() to check each element of the returned array
 
 
-const returnObjects = arrNames => arrNames.map((name, index) => ({name, id: index+1}));
+const returnObjects = arrNames => arrNames.map((name, index) => ({ name, id: index+1 }));
 
-// Test function removeBMW()
-// Make sure you throw something
 
-function removeBMW(str){
-    if (typeof str !== 'string') {
-        throw new Error("Error: You should only enter text.");
-    }
-    return str.replace(/[bmw]/gi,'');
-}
-
-module.exports = { minVal, fizzBuzz, timesTwo, returnObjects, removeBMW};
+module.exports = { minVal, fizzBuzz, timesTwo, returnObjects };
--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,6 @@
-const { minVal, fizzBuzz, timesTwo, returnObjects, removeBMW } = require('./index');
+const { minVal, fizzBuzz, timesTwo } = require('./index');
 
+// A smoke test in real life has just one simple assertion. This is just an example.
 
 test('passes when value is NaN', () => {
   expect(NaN).toBeNaN();
@@ -10,116 +11,24 @@ test('passes when value is NaN', () => {
 
 describe('Testing function minVal()', () => {
 
-    it('Should return a value of data type number', () => {
-        expect(typeof minVal([1,2,34,0,-12])).toBe('number');
-        expect(minVal([1,2,34,0,-12])).not.toBeInstanceOf(Object);
-    });
-    it('Should get a minimal value from an array', () => {
-        expect(minVal([2,-3,4,-5,10,4,-12])).toBe(-12);
-        expect(minVal([1,2,34,0,-12])).toBe(-12);
-    });
 });
 
-describe('Testing function minVal()', () => {	
-	it('Shoul return a value of data type number', () => {
-		expect(typeof minVal([1, 2, 34, 0, -12])).toBe('number');
-		expect(minVal([1, 2, 34, 0, -12])).not.toBeInstanceOf(Object);
-	});
-    it('Should get a minimal value from an array', () => {
-        expect(minVal([1,5,13,-10,-12])).toBe(-12);
-    });   
-    it('Should return a value of data type number', () => {
-        expect(typeof minVal([1,2,34,0,-12])).toBe('number');           
-        expect(minVal([2, -1,3])).toBe(-1);
-    });
-    it('Should throw error message if array is empty', () => {
-        expect(() => {
-            minVal([])
-        }).toThrow("Please enter a parameter");
-    });
-});
-
-describe('Testing function timesTwo()', () => {
-
-    it('returns an array', () => {
-        expect(timesTwo([2.5,2])).toBeInstanceOf(Array);
-    });
-    it('returns an array where numbers are doubled', () => {
-        expect(timesTwo([2.5,2])).toStrictEqual([5,4]);
-    });
-    it('returns an array where numbers are doubled', () => {
-        expect(timesTwo([1,2])).toEqual([2,4]);
-    });
-    it('returns an array where numbers are doubled', () => {
-        expect(timesTwo([1,2])).not.toEqual([5,4]);
-    });
-    it('returns an array where numbers are doubled', () => {
-        expect(timesTwo([1,2])).toBeInstanceOf(Array);
-    });    
+describe('Testing function timesTwo()', () => {   
 
 });
 
 describe('Testing function fizzBuzz()', () => {
 
-	it('returns an array of the same length as a parameter', () => {
-		expect(fizzBuzz(10)).toHaveLength(10);
-	});
-	it('returns an array with some fizz, buzz and fizzbuzz', () => {
-		expect(fizzBuzz(3)).toContain('fizz');
-		expect(fizzBuzz(5)).toContain('buzz');
-		expect(fizzBuzz(15)).toContain('fizzbuzz');
-	});
 });
 
+// Make the test below pass by writing a function
 
-describe('Testing function returnObjects()', () => {
-    it('Should return array of objects', () => {
-        expect(returnObjects(['hi','bye'])).toBeInstanceOf(Object);
-        expect(returnObjects(['hi','bye'])).toBeInstanceOf(Array);
-        expect(returnObjects(['hi','bye'])[0]).toHaveProperty('name');
-        expect(returnObjects(['hi','bye'])[1]).toHaveProperty('id');
-    });
-});
-
-describe('testing removeBMW()', () => {
-    it('Should throw an error if parameter is the wrong type', () => {
-        expect(() => {
-            removeBMW(1010101010);
-        }).toThrow("Error: You should only enter text");
-    });
-	it('should remove bmw', () => {
-		expect(removeBMW('Nobmw')).toBe('No');
-	});
-});
-
-describe('Testing returnobjects()', () => {
-	it('should return an array', () => {
-		expect(returnObjects(['a', 'b'])).toBeInstanceOf(Object);
-		expect(returnObjects(['a', 'b'])[0]).toHaveProperty('name');
-	});
-
-    it('returns an array of the same length as a parameter', () => {
-        expect(fizzBuzz(10)).toHaveLength(10);
-    });
-    it('returns an array with some fizz, buzz and fizzbuzz', () => {
-        expect(fizzBuzz(3)).toContain('fizz');
-        expect(fizzBuzz(15)).toContain('fizzbuzz');        
-    })
-});
-
-//Work done in pairs in a break room
-describe('Testing function returnObject', () => {
-    it('Should return an array', () => {
-        expect(returnObjects(['a','b','c'])).toBeInstanceOf(Array); 
+describe('Testing function that you have not written yet()', () => {
+    test('Function that you have not written yet returns...', () =>{
+        expect(haveNotWrittenYet(9)).toBe("🎄🎄🎄");
+        expect(haveNotWrittenYet(36)).toBe("🎄🎄🎄🎄🎄🎄");
     });
     
-    it('Each element of array should be an Object with name and id', () => {
-      arrObj = returnObjects(['a','b','c'])
-      for (i in arrObj){
-        expect(returnObjects(['a','b','c'])[i]).toHaveProperty('name');
-        expect(returnObjects(['a','b','c'])[i]).toHaveProperty('id');
-        expect(returnObjects(['a','b','c'])[i]).toBeInstanceOf(Object);
-        expect(returnObjects(['a','b','c'])[i]).not.toBeInstanceOf(Array);
-      }
-    });
 });
+
+// Write more functions and more tests. Success criteria: funny and Christmas
